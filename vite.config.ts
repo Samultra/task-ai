e163d86cd494b12c2d884116585c56b8c8a2f5d6
samultra <samultra7@gmail.com>
2025-09-10 21:57:51 +0300

    fix MIME type issue with relative paths and proper headers

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/task-sage-sidekick-new/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
