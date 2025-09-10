ac0612fc6c9cdcf1a2dba4d9dba62eaf7b08fe0e
samultra <samultra7@gmail.com>
2025-09-10 21:29:36 +0300

    configure for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/task-sage-sidekick/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
